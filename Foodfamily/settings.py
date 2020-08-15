338c63bb6aa04765d2c19c76dc44282e08f64cb3
piyushsharma14 <69628515+piyushsharma14@users.noreply.github.com>
2020-08-15 15:01:23 +0530

    Update settings.py

--- Foodfamily/settings.py
+++ Foodfamily/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Foodfamily.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
